e976327b27944b1ea24af5b14d2e2e777cf2f57e
PMTSantos <81629693+PMTSantos@users.noreply.github.com>
2022-12-26 11:26:01 +0000

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const bodyParser = require('body-parser');
 const app = express();
 
 const con = require('./handlers/mysql');
+/*
+* A função abaixo for criada para ao invés de usar as callback functions ser possível designar o valor pedido a uma variável diminuindo as linhas de códido
+*/
 global.con = (sql, values = []) => {
     return new Promise((resolve, reject) => {
         con(sql, values, (err, result) => {
@@ -19,10 +22,10 @@ app.use(bodyParser.json());
 
 app.use('/valdorio', require('./routes/valdorio'));
 
-app.get('*', (req, res) => { // in case none of the routes match
+app.get('*', (req, res) => { // para todos os outros endpoints será enviado um erro 404
     res.status(404).send('Not found');
 });
 
-app.listen(3000, () => {
+app.listen(3000, () => { // A API está a ser executada na porta 3000
     console.log('Listening on port 3000');
 })
